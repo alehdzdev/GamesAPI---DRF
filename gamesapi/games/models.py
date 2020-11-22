4f796e290ac3bfcd5368a6b7b89e2deccd1aee60
Alejandro Hernandez <alejandrohdz.dev@gmail.com>
2020-11-22 14:47:57 -0400

    Adding unique parameters
    Unique parameters added to:
    - Platform
    - Genre

--- gamesapi/games/models.py
+++ gamesapi/games/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Platform(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class Publisher(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
