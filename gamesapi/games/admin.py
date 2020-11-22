10d2135b85006e6fe5360a14bbd5c13482fd07c9
Alejandro Hernandez <alejandrohdz.dev@gmail.com>
2020-11-22 14:51:26 -0400

    Register models to admin site

--- gamesapi/games/admin.py
+++ gamesapi/games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from games.models import Platform, Developer, Genre, Publisher, Game
 
-# Register your models here.
+admin.site.register(Platform)
+admin.site.register(Developer)
+admin.site.register(Genre)
+admin.site.register(Publisher)
+admin.site.register(Game)
