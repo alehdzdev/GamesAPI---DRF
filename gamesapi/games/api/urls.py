6bad770827581f26f2988d38703f3ab151699b2c
Alejandro Hernandez <alejandrohdz.dev@gmail.com>
2020-11-25 15:57:08 -0400

    Url added for developer detail view

--- gamesapi/games/api/urls.py
+++ gamesapi/games/api/urls.py
@@ -4,6 +4,7 @@ from games.api import views as gv
 urlpatterns = [
     path("platforms/", gv.PlatformListAPIView.as_view(),name="platforms-list"),
     path("developers/", gv.DeveloperListAPIView.as_view(),name="developers-list"),
+    path("developers/<pk>/", gv.DeveloperDetailAPIView.as_view(),name="developer-detail"),
     path("genres/", gv.GenreListAPIView.as_view(),name="genres-list"),
     path("publisher/", gv.PublisherListAPIView.as_view(),name="publishers-list"),
     path("games/", gv.GameListAPIView.as_view(),name="games-list"),
