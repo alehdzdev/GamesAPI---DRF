8f760730d70af10cda9212e566fea0b9c3a7bc0f
Alejandro <alejandrohdz.dev@gmail.com>
2021-01-08 17:09:49 -0400

    Update publishers endpoint url

--- gamesapi/games/api/urls.py
+++ gamesapi/games/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("developers/", gv.DeveloperListAPIView.as_view(),name="developers-list"),
     path("developers/<pk>/", gv.DeveloperDetailAPIView.as_view(),name="developer-detail"),
     path("genres/", gv.GenreListAPIView.as_view(),name="genres-list"),
-    path("publisher/", gv.PublisherListAPIView.as_view(),name="publishers-list"),
+    path("publishers/", gv.PublisherListAPIView.as_view(),name="publishers-list"),
     path("games/", gv.GameListAPIView.as_view(),name="games-list"),
 ]
