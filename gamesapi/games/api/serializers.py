33e12f88612dafdd15eb3735c5e929169b28c1d2
Alejandro <alejandrohdz.dev@gmail.com>
2021-01-08 16:36:33 -0400

    Update Game serializer

--- gamesapi/games/api/serializers.py
+++ gamesapi/games/api/serializers.py
@@ -33,8 +33,8 @@ class PublisherSerializer(serializers.ModelSerializer):
 class GameSerializer(serializers.ModelSerializer):
 
     created_at = serializers.SerializerMethodField()
-    developer = DeveloperSerializer(read_only=True)
-    publisher = PublisherSerializer(read_only=True)
+    developers = DeveloperSerializer(many=True, read_only=True)
+    publishers = PublisherSerializer(many=True, read_only=True)
     genres = GenreSerializer(many=True, read_only=True)
     platforms = PlatformSerializer(many=True, read_only=True)
 
