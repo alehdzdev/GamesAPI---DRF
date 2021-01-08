058c4f921bfd928dc0e1d3c4dac62401b33ce1ba
Alejandro <alejandrohdz.dev@gmail.com>
2021-01-08 16:26:42 -0400

    Update Game model

--- gamesapi/games/models.py
+++ gamesapi/games/models.py
@@ -33,14 +33,11 @@ class Game(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
+    release_date = models.DateField()
     genres = models.ManyToManyField(Genre, related_name="genres")
     platforms = models.ManyToManyField(Platform, related_name="platforms")
-    developer = models.ForeignKey(Developer,
-                                  on_delete=models.CASCADE,
-                                  related_name="developer")
-    publisher = models.ForeignKey(Publisher,
-                                  on_delete=models.CASCADE,
-                                  related_name="publisher")
+    developers = models.ManyToManyField(Developer, related_name="developers")
+    publishers = models.ManyToManyField(Publisher, related_name="publishers")
     
     def __str__(self):
         return self.name
