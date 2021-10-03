2eb76c2bddcabb896f9dbceb2e67ff17d6685505
Alejandro Hernandez <alejandrohdz.dev@gmail.com>
2021-10-03 13:30:11 -0400

    ADD docstings to serializers

--- games/api/serializers.py
+++ games/api/serializers.py
@@ -1,36 +1,38 @@
+"""Games serializers."""
 from rest_framework import serializers
 from games.models import Platform, Developer, Genre, Publisher, Game
 
 
 class PlatformSerializer(serializers.ModelSerializer):
-
+    """Platform serializer."""
     class Meta:
         model = Platform
         fields = "__all__"
 
 
 class DeveloperSerializer(serializers.ModelSerializer):
-
+    """Developer serializer."""
     class Meta:
         model = Developer
         fields = "__all__"
 
 
 class GenreSerializer(serializers.ModelSerializer):
-
+    """Genre serializer."""
     class Meta:
         model = Genre
         fields = "__all__"
 
 
 class PublisherSerializer(serializers.ModelSerializer):
-
+    """Publisher serializer."""
     class Meta:
         model = Publisher
         fields = "__all__"
 
 
 class GameSerializer(serializers.ModelSerializer):
+    """Game serializer with the corresponded serializer as fields"""
 
     created_at = serializers.SerializerMethodField()
     developers = DeveloperSerializer(read_only=True)
