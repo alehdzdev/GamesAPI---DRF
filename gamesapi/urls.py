364b8f3f401c460d8e58d95aeb75d38044a4bc69
Alejandro Hernandez <alejandrohdz.dev@gmail.com>
2021-10-03 13:30:50 -0400

    UPDATE urls names

--- gamesapi/urls.py
+++ gamesapi/urls.py
@@ -1,18 +1,4 @@
-"""gamesapi URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""URLs configuration GamesAPI"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,6 +9,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from games.views import HomePageView
+
+admin.site.site_header = "GamesAPI"
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -42,8 +30,8 @@ urlpatterns = [
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('game-manage/', admin.site.urls),
     path('api/v1/', include('games.api.urls')),
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
